f864e6ba8bc853acb0ce1ea4ee7c8355060701a7
Wilfred Morgan <wil.me.morgan@gmail.com>
2021-09-14 00:16:02 +0800

    Implement the following methods: - Delete - Update - Save - GetContactById - GetAllContacts

---
+++ ContactManagerApi/Services/ContactService.cs
@@ -0,0 +1,67 @@
+﻿using ContactManagerApi.Data;
+using ContactManagerApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactManagerApi.Services
+{
+    public class ContactService : IContactService
+    {
+        private IContactRepository contactRepository;
+
+        public List<Contact> GetAllContacts()
+        {
+            List<Contact> rtnList = new List<Contact>();
+
+            foreach(Contact contact in contactRepository.FindAll())
+            {
+                rtnList.Add(contact);
+            }
+
+            return rtnList;
+        }
+
+        public Contact GetContactById(int id)
+        {
+            return contactRepository.FindOne(id);
+        }
+
+        public List<CallListContact> GetCallListContacts()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public int Save(Contact contact)
+        {
+            return contactRepository.Insert(contact);
+        }
+
+        public bool Update(Contact contact, int id)
+        {
+            if (contactRepository.FindOne(id) != null)
+            {
+                return contactRepository.Update(contact);
+            }
+            else
+            {
+                throw new Exception($"Contact {id} not found");
+            }
+        }
+
+        public bool Delete(int id)
+        {
+            if (contactRepository.FindOne(id) != null)
+            {
+                return contactRepository.Delete(id);
+            }
+            else
+            {
+                throw new Exception($"Contact {id} not found");
+            }
+                
+        }
+    }
+}
