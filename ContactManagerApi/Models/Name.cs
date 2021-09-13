116b0d77c735d521457545cd73d207aa961dface
Wilfred Morgan <wil.me.morgan@gmail.com>
2021-09-13 20:41:58 +0800

    Add Name model

---
+++ ContactManagerApi/Models/Name.cs
@@ -0,0 +1,13 @@
+﻿/// <summary>
+/// Class represents name entity
+/// </summary>
+
+namespace ContactManagerApi.Models
+{
+    public class Name
+    {
+        public string First { get; set; }
+        public string Middle { get; set; }
+        public string Last { get; set; }
+    }
+}
