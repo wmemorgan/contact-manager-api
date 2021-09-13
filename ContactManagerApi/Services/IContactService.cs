3dd51bc17f5e60cd5329248f776ee9ecfe65fbd2
Wilfred Morgan <wil.me.morgan@gmail.com>
2021-09-13 23:57:31 +0800

    Define IContactService interface

---
+++ ContactManagerApi/Services/IContactService.cs
@@ -0,0 +1,19 @@
+﻿using ContactManagerApi.Models;
+using System.Collections.Generic;
+
+/// <summary>
+/// The service that works with the Contacts and CallListContact models
+/// </summary>
+
+namespace ContactManagerApi.Services
+{
+    public interface IContactService
+    {
+        IEnumerable<Contact> GetAllContacts();
+        Contact GetContactById(int id);
+        List<CallListContact> GetCallListContacts();
+        int Save(Contact contact);
+        bool Update(Contact contact, int id);
+        bool Delete(int id);
+    }
+}
