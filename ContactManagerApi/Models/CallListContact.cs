09fca984860bfc5ffe9fba4ed6dd8a3cd76f9a93
Wilfred Morgan <wil.me.morgan@gmail.com>
2021-09-13 23:57:04 +0800

    Declare CallListContact model

---
+++ ContactManagerApi/Models/CallListContact.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactManagerApi.Models
+{
+    public class CallListContact
+    {
+        public Name name;
+
+        [Display(Name = "phone")]
+        public string PhoneNumber { get; set; }
+    }
+}
