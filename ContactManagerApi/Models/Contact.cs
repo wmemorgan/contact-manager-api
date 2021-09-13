f34bd078a2975c8dc09f81769678d6f20e786b90
Wilfred Morgan <wil.me.morgan@gmail.com>
2021-09-13 21:30:06 +0800

    Create Contact model

---
+++ ContactManagerApi/Models/Contact.cs
@@ -0,0 +1,16 @@
+﻿/// <summary>
+/// This class represents the contact entity
+/// </summary>
+
+using System.Collections.Generic;
+
+namespace ContactManagerApi.Models
+{
+    public class Contact
+    {
+        public Name name;
+        public Address address; 
+        public List<Phone> phone = new List<Phone>(); 
+        public string Email { get; set; }
+    }
+}
