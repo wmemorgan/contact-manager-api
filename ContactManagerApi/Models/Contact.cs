1e47ec0cbb249d40b23fe02f1e8d4fa9ecb1d07f
Wilfred Morgan <wil.me.morgan@gmail.com>
2021-09-14 14:12:54 +0800

    Add getters and setters to properties

--- ContactManagerApi/Models/Contact.cs
+++ ContactManagerApi/Models/Contact.cs
@@ -9,9 +9,9 @@ namespace ContactManagerApi.Models
     public class Contact
     {
         public int Id { get; set; }
-        public Name name;
-        public Address address; 
-        public List<Phone> phone = new List<Phone>(); 
+        public Name name { get; set; }
+        public Address address { get; set; } 
+        public List<Phone> phone { get; set; } = new List<Phone>(); 
         public string Email { get; set; }
     }
 }
