eafcba979f45f0cb6575d6662a0d36eeffb7eb8c
Wilfred Morgan <wil.me.morgan@gmail.com>
2021-09-14 16:43:24 +0800

    Create method to setup test db

---
+++ ContactManagerApiTests/Data/ContactRepositoryTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LiteDB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ContactManagerApi.Models;
+
+namespace ContactManagerApiTests.Data
+{
+    [TestClass]
+    class ContactRepositoryTests
+    {
+        public LiteDatabase SetupTestDb()
+        {
+            var db = new LiteDatabase("Data/TestContactsDb");
+            var col = db.GetCollection<Contact>("contacts");
+
+            return db;
+        }
+
+    }
+}
