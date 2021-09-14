6ff8edd4cd466c76d2dc22e85562e06223340316
Wilfred Morgan <wil.me.morgan@gmail.com>
2021-09-14 16:54:08 +0800

    Add helper method to create test contact

--- ContactManagerApiTests/Data/ContactRepositoryTests.cs
+++ ContactManagerApiTests/Data/ContactRepositoryTests.cs
@@ -20,5 +20,44 @@ namespace ContactManagerApiTests.Data
             return db;
         }
 
+        public Contact CreateTestContact()
+        {
+            Name name = new Name
+            {
+                First = "Harold",
+                Middle = "Francis",
+                Last = "Gikey"
+            };
+
+            Address address = new Address
+            {
+                Street = "8360 High Autumn Row",
+                City = "Cannon",
+                State = "Delaware",
+                Zip = "19797"
+            };
+
+            Contact contact = new Contact
+            {
+                name = name,
+                address = address,
+                Email = "harold.gikey@yahoo.com"
+            };
+
+            contact.phone.Add(new Phone
+            {
+                Number = "302-611-9148",
+                Type = "home"
+            });            
+            
+            contact.phone.Add(new Phone
+            {
+                Number = "302-532-9427",
+                Type = "mobile"
+            });
+
+            return contact;
+        }
+
     }
 }
