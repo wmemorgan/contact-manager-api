84f153e5096d85da63ec37099524a31ee390b610
Wilfred Morgan <wil.me.morgan@gmail.com>
2021-09-15 21:59:37 +0800

    Add data annotation

--- ContactManagerApi/Models/Contact.cs
+++ ContactManagerApi/Models/Contact.cs
@@ -3,12 +3,15 @@
 /// </summary>
 
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ContactManagerApi.Models
 {
     public class Contact
     {
         public int Id { get; set; }
+        
+        [Required]
         public Name name { get; set; }
         public Address address { get; set; } 
         public List<Phone> phone { get; set; } = new List<Phone>(); 
